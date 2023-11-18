c07c9c11a3c85ddf5f5008cd0292ff487e68aca7
ulvifk <92516870+ulvifk@users.noreply.github.com>
2023-11-19 00:29:27 +0300

    Revert "hate rebasing"
    This reverts commit 40f7aac1a31e350f65d2bd1a8e4cf1abb8651e2f.

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+import heuristics.neighborhoodSearch.IntraSwapSearch;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        var in = new IntraSwapSearch(null);
+    }
+}
