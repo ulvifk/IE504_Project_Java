ff3538aaa144878408265359c6e1dc00037dc7c1
ulvifk <92516870+ulvifk@users.noreply.github.com>
2023-11-18 19:40:16 +0300

    tabu-search and some neighborhood search functions

--- java/main.java
+++ java/main.java
@@ -1,5 +1,9 @@
+import heuristics.neighborhoodSearch.IntraSwapSearch;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        var in = new IntraSwapSearch(null);
     }
 }
