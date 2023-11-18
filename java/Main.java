40f7aac1a31e350f65d2bd1a8e4cf1abb8651e2f
ulvifk <92516870+ulvifk@users.noreply.github.com>
2023-11-19 00:01:51 +0300

    hate rebasing

--- java/Main.java
+++
@@ -1,9 +0,0 @@
-import heuristics.neighborhoodSearch.IntraSwapSearch;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        var in = new IntraSwapSearch(null);
-    }
-}
